dc77ccf787fb61efa360e3ae93bf2516ffe58ae4
Divya <java.divya.garg@gmail.com>
2019-11-11 13:00:00 +0530

    Commit

--- SpringIocDemo_1/src/main/java/com/iris/Main.java
+++ SpringIocDemo_1/src/main/java/com/iris/Main.java
@@ -19,10 +19,10 @@ public class Main {
 	lObj2.printLaptopDetails();
 	
 	Laptop lObj3=context.getBean("obj3",Laptop.class);
-	lObj3.printLaptopDetails();*/
-	
-	Car car=(Car)context.getBean("cObj1");
-	System.out.println(car);
+	lObj3.printLaptopDetails();
+	*/
+	/*Car car=(Car)context.getBean("cObj1");
+	System.out.println(car);*/
 	
 	}
 
